7098571ee74fd806b922c044349496c786ca147e
MarioMarki <markimario27@gmail.com>
2026-09-02 11:16:51 +0200

    Reverting after test

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ DTSTART:{ics_stamp(ses['date'],ses['time'])}
 DURATION:{length}
 SUMMARY:{summary(race["raceName"],session)}
 BEGIN:VALARM
-TRIGGER:-PT1H
+TRIGGER:-PT30M
 ACTION:DISPLAY
 DESCRIPTION:Session starting soon
 END:VALARM
