8b65f0d5342fbc5a4c1eff61e4db81fd2f96fc28
MarioMarki <markimario27@gmail.com>
2026-09-25 19:00:46 +0200

    Add MotoGP calendar, rename f1.ics to F1.ics

--- Series/MotoGP.py
+++ Series/MotoGP.py
@@ -2,18 +2,50 @@ import requests
 from datetime import datetime,timezone
 
 data = requests.get("https://api.pulselive.motogp.com/motogp/v1/events?seasonYear=2026").json()
-# i=0
-# for item in data:
-#     if item["kind"] == "GP":
-#         broadcast = item["broadcasts"]
-#         for event in broadcast:
-#             if event["category"]["acronym"] == "MGP":
-#                 if event["kind"] == "QUALIFYING" or event["kind"] == "RACE":
-#                     i+=1
-# print(i)
+LENGHTS = {
+    "FP1":"PT45M",
+    "PR":"PT1H",
+    "FP2":"PT30M",
+    "Q1":"PT15M",
+    "Q2":"PT15M",
+    "SPR":"PT20M",
+    "RAC":"PT40M",
+    "RAC2":"PT40M",
+    "RAC3":"PT40M",
+}
+NAMES = {
+    "Free Practice Nr. 1":"FP1",
+    "Free Practice Nr. 2":"FP2",
+    "Qualifying Nr. 1 ":"Q1",
+    "Qualifying Nr. 2 ":"Q2",
+    "Grand Prix":"Race"
+}
 
 def iso_to_ics_stamp(time):
     parsed_dt = datetime.fromisoformat(time)
     utc_dt = parsed_dt.astimezone(timezone.utc)
     result = utc_dt.strftime('%Y%m%dT%H%M%SZ')
     return result
+
+def get_session():
+    result = []
+    for item in data:
+        if item['kind'] == "GP":
+            broadcast = item['broadcasts']
+            for event in broadcast:
+                if event['kind'] == "PRACTICE" or event['kind'] == "QUALIFYING" or event['kind'] == "RACE":
+                    if event["category"]["acronym"] == "MGP":
+                        duration = f"{LENGHTS.get(event['shortname'], 'PT1H')}"
+                        uid = f"{event['id']}"
+                        start = f"{iso_to_ics_stamp(event['date_start'])}"
+                        title = f"{event['category']['name']} - {item['hashtag'].replace('#','')} - {NAMES.get(event['name'],event['name'])}"
+                        summary = {
+                            'uid': uid,
+                            'start': start,
+                            'duration': duration,
+                            'title': title,
+                        }
+                        result.append(summary)
+    return result
+
+print(len(get_session()))
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from Series.F1 import get_session
+from Series import F1
+from Series import MotoGP
 import ics
-sessions = get_session()
-with open("ICS Files/f1.ics", "w",encoding="utf-8") as ics_file:
+sessions_f1 = F1.get_session()
+sessions_motoGP = MotoGP.get_session()
+with open("ICS Files/F1.ics", "w",encoding="utf-8") as ics_file:
     ics_file.write(f"{ics.header}\n")
-    for session in sessions:
+    for session in sessions_f1:
+        ics_file.write(f"{ics.event_block(**session)}\n")
+    ics_file.write(ics.footer)
+
+with open("ICS Files/MotoGP.ics", "w",encoding="utf-8") as ics_file:
+    ics_file.write(f"{ics.header}\n")
+    for session in sessions_motoGP:
         ics_file.write(f"{ics.event_block(**session)}\n")
     ics_file.write(ics.footer)
